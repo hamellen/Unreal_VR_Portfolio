85a6ee9f09e35d733a50c8a9853df9a14c474111
hamellen <zzodong0309@naver.com>
2025-02-23 01:03:42 +0900

    code #1

---
+++ SQUAD/Source/SQUAD/Weapon/Rifle.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Rifle.generated.h"
+
+UCLASS(Abstract)
+class SQUAD_API ARifle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARifle();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	UPROPERTY(EditAnywhere,Category= GUN)
+	int32 current_ammo;
+
+	UPROPERTY(EditAnywhere, Category = GUN)
+	int32 max_ammo;
+
+public:
+
+	virtual void Weapon_Fire() PURE_VIRTUAL(ARifle::Weapon_Fire, );
+
+};
